bf46f6b690de9ad8dce77ef4996d648d12ecfc00
sauttets <tobias.sautter@gmail.com>
2023-11-23 12:10:01 +0100

    prettied index page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,49 +1,55 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
+
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'MQTT API',
+    Svg: require('@site/static/img/mqtt-api.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        The MQTT API is a simple and lightweight messaging protocol that runs over TCP/IP sockets or WebSockets.
       </>
     ),
+    link: 'docs/user-api/mqtt-api/intro',
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'REST & WebSocket API',
+    Svg: require('@site/static/img/rest-api.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        The REST & WebSocket API is a simple and lightweight messaging protocol that runs over HTTP or WebSockets.
       </>
     ),
+    link: '/docs/category/rest--websocket-api',
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Docs',
+    Svg: require('@site/static/img/docs.svg').default,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
         be extended while reusing the same header and footer.
       </>
     ),
+    link: '/docs/intro',
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({Svg, title, description, link}) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col col--4')}>  
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <Link to={link}>
+          <Svg className={styles.featureSvg} role="img" />
+        </Link>
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
+        <Link to={link} className="button button--secondary myButton">Learn More</Link>
       </div>
     </div>
   );
@@ -62,3 +68,5 @@ export default function HomepageFeatures() {
     </section>
   );
 }
+
+
