56419ff3a2df3410863f4e979f3165203bc9ea35
sauttets <tobias.sautter@gmail.com>
2023-11-28 13:38:32 +0100

    404 page

---
+++ src/theme/NotFound/Content/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import clsx from 'clsx';
+import Translate from '@docusaurus/Translate';
+import Heading from '@theme/Heading';
+import jellyImg from '/static/img/logo_jelly_idk-panic.png';
+export default function NotFoundContent({className}) {
+  return (
+    <main className={clsx('container margin-vert--xl', className)}>
+      <div className="row">
+        <div className="col col--6 col--offset-1">
+          <Heading as="h1" className="hero__title">
+            <Translate
+              id="theme.NotFound.title"
+              description="The title of the 404 page">
+              Page Not Found
+            </Translate>
+          </Heading>
+          <p>
+            <Translate
+              id="theme.NotFound.p1"
+              description="Something went wrong!">
+              We could not find what you were looking for.
+            </Translate>
+          </p>
+          <p>
+            <Translate
+              id="theme.NotFound.p2"
+              description="The 2nd paragraph of the 404 page">
+              Please contact the owner of the site that linked you to the
+              original URL and let them know their link is broken.
+            </Translate>
+          </p>
+        </div>
+        <div className="col col--3">
+          <p>
+            <img src={jellyImg} alt="Image" height="300" />
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
